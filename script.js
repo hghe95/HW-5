fe0f2b9c6609cab3205a27da314d2a422f53d59a
hghe95 <hghe95@gmail.com>
2022-12-22 14:43:53 -0800

    completely revamped the js file

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let currentTime = moment().format("");
+let currentTime = moment().hour()
 const saveBtn = document.querySelectorAll(`.saveBtn`);
+document.querySelector(`#currentDay`).textContent = moment().format(` MMM Do YYYY`);
+const militaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-const changeColor = () => {
-    let currentHour = moment().hours();
+
+localStorage.getItem(`myEvent`);
+for (let i = 0; i < militaryTime.length; i++) {
+    if (currentTime > militaryTime[i]) {
+        console.log(`Current hour is ${i}`);
+        
+    }
+    if (currentTime === militaryTime[i]) {
+        console.log(`Current hour is ${i}`);
+
+    }
+    if (currentTime < militaryTime[i]) {
+        console.log(`Current hour is ${i}`);
+    }
 }
 
-const renderText = () => {
-    
-}
 
 
 
