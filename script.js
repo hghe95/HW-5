f42bacb9ffa44bb59b81a364a4b4dfefab9a1702
hghe95 <hghe95@gmail.com>
2022-12-22 19:41:28 -0800

    added new code to script

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 let currentTime = moment().hour()
-const saveBtn = document.querySelectorAll(`.saveBtn`);
+const saveBtn = document.querySelector(".saveBtn");
 document.querySelector(`#currentDay`).textContent = moment().format(`MMM Do YYYY`);
 const militaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-localStorage.getItem(`scheduledEvent`);
+
 
 for (let i = 0; i < militaryTime.length; i++) {
     if (currentTime > militaryTime[i]) {
-        console.log(`Current hour is ${i}`);
+        console.log(i);
         document.getElementById(i).addClass("past");
     } else if (currentTime === militaryTime[i]) {
-        console.log(`Current hour is ${i}`);
+        console.log(i);
         document.getElementById(i).addClass("present");
     } else if (currentTime < militaryTime[i]) {
-        console.log(`Current hour is ${i}`);
+        console.log(i);
         document.getElementById(i).addClass("future");
     }
 
     if (localStorage.getItem(i)) {
         document.getElementById(i).value = localStorage.getItem(i);
     }
+
+    saveBtn[i].addEventListener("click", function(event) {
+        event.preventDefault;
+        var id = this.getAttribute("id");
+        var text = document.getElementById(id).value;
+        localStorage.setItem(id, text);
+    });
+
+    if(localStorage.getItem(i)) {
+        document.getElementById(i).value = localStorage.getItem(i);
+    }
 }
 
-saveBtn.addEventListener("click", function(event) {
-    let x = event.target;
-    console.log(x);
-    localStorage.setItem("scheduledEvent", $(`textarea`))
-})
+
 
 
 
